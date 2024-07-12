6284e4ab47a33ad09b88605317dc824485f5ea98
Pritam <pritambasu14@gmail.com>
2024-07-12 18:16:12 +0530

    feat: add code for creating users

--- controllers/users.go
+++ controllers/users.go
@@ -3,12 +3,15 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/soppydart/Photog-Burst/models"
 )
 
-type Users struct{
+type Users struct {
 	Templates struct {
 		New Template
 	}
+	UserService *models.UserService
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +23,13 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Email: ", r.FormValue("email"))
-	fmt.Fprint(w, "Password: ", r.FormValue("password"))
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	user, err := u.UserService.Create(email, password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong!", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User Created!: %+v", user)
 }
