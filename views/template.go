d276016910a0d3230ee80eaa99c320d3ba0ca17c
Pritam <pritambasu14@gmail.com>
2024-07-15 00:51:02 +0530

    feat: add template function to check whether a user is signed in or not

--- views/template.go
+++ views/template.go
@@ -10,6 +10,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/csrf"
+	"github.com/soppydart/Photog-Burst/context"
+	"github.com/soppydart/Photog-Burst/models"
 )
 
 type Template struct {
@@ -22,6 +24,9 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 		template.FuncMap{
 			"csrfField": func() (template.HTML, error) {
 				return "", fmt.Errorf("csrfField not implemented")
+			},
+			"currentUser": func() (template.HTML, error) {
+				return "", fmt.Errorf("currentUser not implemented")
 			},
 		},
 	)
@@ -63,6 +68,9 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 		template.FuncMap{
 			"csrfField": func() template.HTML {
 				return csrf.TemplateField(r)
+			},
+			"currentUser": func() *models.User {
+				return context.User(r.Context())
 			},
 		},
 	)
