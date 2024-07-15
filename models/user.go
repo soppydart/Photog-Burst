aafe666c79144a70a1e1c559d8c958b9158fb81e
Pritam <pritambasu14@gmail.com>
2024-07-16 01:57:57 +0530

    feat: add a function on the user service to update the user's password

--- models/user.go
+++ models/user.go
@@ -66,3 +66,23 @@ func (us *UserService) Authenticate(email, password string) (*User, error) {
 
 	return &user, nil
 }
+
+func (us *UserService) UpdatePassword(userId int, password string) error {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password),
+		bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("update password: %w", err)
+	}
+	passwordHash := string(hashedBytes)
+
+	_, err = us.DB.Exec(`
+		UPDATE users
+		SET password_hash = $2
+		WHERE id = $1;
+	`, userId, passwordHash)
+
+	if err != nil {
+		return fmt.Errorf("update password: %w", err)
+	}
+	return nil
+}
