bb77b2eade63e7a4bbb885d31301a825ca772065
Pritam <pritambasu14@gmail.com>
2024-07-17 23:36:53 +0530

    feat: add gallery controller with functions to handle displaying, updating and deleting user galleries

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,14 +1,23 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/soppydart/Photog-Burst/context"
+	"github.com/soppydart/Photog-Burst/errors"
 	"github.com/soppydart/Photog-Burst/models"
+	"golang.org/x/exp/rand"
 )
 
 type Galleries struct {
 	Templates struct {
+		Show Template
 		New Template
+		Edit Template
+		Index Template
 	}
 	GalleryService *models.GalleryService
 }
@@ -19,4 +28,150 @@ func (g Galleries) New(w http.ResponseWriter, r *http.Request) {
 	}
 	data.Title = r.FormValue("title")
 	g.Templates.New.Execute(w, r, data)
+}
+
+func (g Galleries) Create(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		UserID int
+		Title string
+	}
+	data.UserID = context.User(r.Context()).ID
+	data.Title = r.FormValue("title")
+
+	gallery, err := g.GalleryService.Create(data.Title, data.UserID)
+	if err != nil {
+		g.Templates.New.Execute(w, r, data, err)
+		return
+	}
+	editPath := fmt.Sprintf("galleries/%d/edit", gallery.ID)
+	http.Redirect(w, r, editPath, http.StatusFound)
+}
+
+func (g Galleries) Edit(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.GalleryByID(w, r, UserMustOwnGallery)
+	if err != nil {
+		return
+	}
+	var data struct {
+		ID int
+		Title string
+	}
+	data.ID = gallery.ID
+	data.Title = gallery.Title
+	g.Templates.Edit.Execute(w, r, data);
+}
+
+func (g Galleries) Update(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.GalleryByID(w, r, UserMustOwnGallery)
+	if err != nil {
+		return
+	}
+	gallery.Title = r.FormValue("title")
+	err = g.GalleryService.Update(gallery)
+	if err != nil {
+		http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		return
+	}
+	editPath := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
+	http.Redirect(w, r, editPath, http.StatusFound)
+}
+
+func (g Galleries) Index(w http.ResponseWriter, r *http.Request) {
+	type Gallery struct {
+		ID int
+		Title string
+	}
+	var data struct {
+		Galleries []Gallery
+	}
+
+	user := context.User(r.Context())
+	galleries, err := g.GalleryService.ByUserID(user.ID)
+	if err != nil {
+		http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		return
+	}
+	for _, gallery := range galleries {
+		data.Galleries = append(data.Galleries, Gallery{
+			ID: gallery.ID,
+			Title: gallery.Title,
+		})
+	}
+
+	g.Templates.Index.Execute(w, r, data)
+}
+
+func (g Galleries) Show(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.GalleryByID(w, r)
+	if err != nil {
+		return
+	}
+	var data struct {
+		ID int
+		Title string
+		Images []string
+	}
+	data.ID = gallery.ID
+	data.Title = gallery.Title
+
+	for i:=0; i<20; i++ {
+		w, h := rand.Intn(500) + 200, rand.Intn(500) + 200
+		// catImageURL := "https://api.pexels.com/v1/search?query=nature&per_page=20"
+		catImageURL := fmt.Sprintf("https://images.pexels.com/photos/2880507/pexels-photo-2880507.jpeg?auto=compress&cs=tinysrgb&h=%d&w=%d", w, h)
+		data.Images = append(data.Images, catImageURL)
+	}
+
+	g.Templates.Show.Execute(w, r, data)
+}
+
+type galleryOpt func(http.ResponseWriter, *http.Request, *models.Gallery) error
+
+func (g Galleries) GalleryByID(w http.ResponseWriter, r *http.Request, 
+	opts ...galleryOpt) (*models.Gallery, error){
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusNotFound)
+		return nil, err
+	}
+	gallery, err := g.GalleryService.ByID(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNotFound) {
+			http.Error(w, "Gallery Not Found!", http.StatusNotFound)
+			return nil, err
+		}
+		http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		return nil, err
+	}
+
+	for _, opt := range opts {
+		err = opt(w, r, gallery)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return gallery, nil
+}
+
+func (g Galleries) Delete(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.GalleryByID(w, r, UserMustOwnGallery)
+	if err != nil {
+		return
+	}
+	err = g.GalleryService.Delete(gallery.ID)
+	if err != nil {
+		http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/galleries", http.StatusFound)
+}
+
+func UserMustOwnGallery(w http.ResponseWriter, r *http.Request, 
+	gallery *models.Gallery) error {
+	user :=context.User(r.Context())
+	if(gallery.UserID != user.ID) {
+		http.Error(w, "Not authorized to edit this gallery!", http.StatusForbidden)
+		return fmt.Errorf("user does not have access to this gallery")
+	}
+	return nil
 }
